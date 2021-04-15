63c22a2b9e12f9d5ecf90541efd1e93ba183b77a
Manoel Almeida <manoel.almeida@bandtec.com.br>
2021-04-14 21:25:57 -0300

    App finalizado

--- C2Cachorros/app/src/main/java/br/com/bandtec/c2cachorros/MainActivity.kt
+++ C2Cachorros/app/src/main/java/br/com/bandtec/c2cachorros/MainActivity.kt
@@ -1,11 +1,13 @@
 package br.com.bandtec.c2cachorros
 
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.EditText
+import android.widget.Switch
 import android.widget.Toast
 import br.com.bandtec.c2cachorros.api.Cachorro
 import br.com.bandtec.c2cachorros.api.ConexaoApiCachorros
@@ -15,8 +17,12 @@ import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
+  private lateinit var preferencia: SharedPreferences
+
   lateinit var etCachorro1: EditText
   lateinit var etCachorro2: EditText
+
+  lateinit var scAmigavel: Switch
 
   var id1: Int? = null
   var id2: Int? = null
@@ -28,8 +34,18 @@ class MainActivity : AppCompatActivity() {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
 
+    preferencia = getSharedPreferences("dados", MODE_PRIVATE)
+
     etCachorro1 = findViewById(R.id.et_cachorro1)
     etCachorro2 = findViewById(R.id.et_cachorro2)
+
+    scAmigavel = findViewById(R.id.sc_amigavel)
+
+    val id1 = preferencia.getString("id1", "")
+    val id2 = preferencia.getString("id2", "")
+
+    etCachorro1.setText(id1)
+    etCachorro2.setText(id2)
   }
 
   fun comprar(view: View) {
@@ -41,6 +57,11 @@ class MainActivity : AppCompatActivity() {
       id2 = etCachorro2.text.toString().toInt()
     }
 
+    val edit = preferencia.edit()
+    edit.putString("id1", id1.toString())
+    edit.putString("id2", id2.toString())
+    edit.apply()
+
     pesquisarCachorro(1, id1 ?: 0)
   }
 
@@ -50,16 +71,25 @@ class MainActivity : AppCompatActivity() {
     callback.enqueue(object : Callback<Cachorro> {
       override fun onResponse(call: Call<Cachorro>, response: Response<Cachorro>) {
         if (posicao == 1) {
+          Log.i("response", response.toString())
           if (response.isSuccessful) {
             cachorro1 = response.body()
+            Log.i("CACHORRO 1", cachorro1.toString())
+            if (scAmigavel.isChecked && cachorro1?.indicadoCriancas == false) {
+              cachorro1 = null
+            }
           } else {
             cachorro1 = null
           }
           Log.i("CACHORRO 1", cachorro1.toString())
           pesquisarCachorro(2, id2 ?: 0)
         } else if (posicao == 2) {
+          Log.i("response", response.toString())
           if (response.isSuccessful) {
             cachorro2 = response.body()
+            if (scAmigavel.isChecked && cachorro2?.indicadoCriancas == false) {
+              cachorro2 = null
+            }
           } else {
             cachorro2 = null
           }
